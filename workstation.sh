73d61ba807fce27b0c351075a29c7d3eb6e4ac24
Pavan Kumar Akula <akulapavankumar@outlook.com>
2024-07-26 13:32:48 +0200

    Add instructions for installing helm

--- workstation.sh
+++ workstation.sh
@@ -31,3 +31,8 @@ sudo ln -s /opt/kubectx/kubens /usr/local/bin/kubens
 curl "https://awscli.amazonaws.com/awscli-exe-linux-$(uname -m).zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install
+
+# Install helm
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
