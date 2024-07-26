5f08058d494fae894abf03237f4bf17c1103c02b
Pavan Kumar Akula <akulapavankumar@outlook.com>
2024-07-26 13:01:53 +0200

    Setup workstation

---
+++ workstation.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+#################################
+# Instructions to setup necessary
+# tools on workstation
+
+# Tested with CentOS 9 AMI
+#################################
+
+# Install git and unzip packages
+dnf install git unzip -y
+
+ARCH=amd64
+PLATFORM=$(uname -s)_$ARCH
+
+# Install kubectl
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+chmod +x kubectl
+mv kubectl /usr/local/bin/kubectl
+
+# Install eksctl
+curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
+tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
+sudo mv /tmp/eksctl /usr/local/bin
+
+# Setup kubectx and kubens
+sudo git clone https://github.com/ahmetb/kubectx /opt/kubectx
+sudo ln -s /opt/kubectx/kubens /usr/local/bin/kubens
+
+# Install AWS CLI
+curl "https://awscli.amazonaws.com/awscli-exe-linux-$(uname -m).zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
